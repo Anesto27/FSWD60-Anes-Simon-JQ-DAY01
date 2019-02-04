f8ac525365470a8dbecd867eeb6861bdad9c72d2
IamMoony <s.blaha@live.at>
2019-02-04 10:53:58 +0100

    upload intermediate exercise 2

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,16 @@
-$("form").submit(function(e) {
+$(document).ready(function(){
+	$("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
-	
-
 });
+
+// $("form").remove();
+for (i=1; i<6; i++) {
+$("article").append("<div>"+i+"</div>");
+}
+
+var n = "Search for ..."
+
+$("input#msg").value(n);
+})
+
