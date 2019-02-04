e92704120a0412336a09c162d31ebe531d60468e
IamMoony <s.blaha@live.at>
2019-02-04 11:22:12 +0100

    modify intermediate exercise part 5 and add advanced exercise

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -14,11 +14,10 @@ $("article div:eq("+(i-1)+")").addClass("box");
 
 $("input#msg").attr("value", "Search for ...");
 
-<<<<<<< HEAD
-$("a").attr("href", "https://www.codefactory.wien");
-=======
 
->>>>>>> 16b4baa0372a01988a47e0751e295779b474b685
+$("a").attr("href", "https://www.codefactory.wien");
+
+
 
 })
 
