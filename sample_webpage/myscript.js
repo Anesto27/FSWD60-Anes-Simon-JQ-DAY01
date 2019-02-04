b3d586ab5ab3aacf2da22e0ce82bd76451bb63fe
JamalAhmed2019 <jamalusb@gmx.at>
2019-02-04 13:07:06 +0100

    manipulation zhe background of the page

--- sample_webpage/myscript.js
+++ sample_webpage/myscript.js
@@ -7,3 +7,7 @@ $("ol>li").css("background-color","#2a7b90");
 //4
 $("form textarea").attr("placeholder","Express your opinion");
 //5
+$("img").attr("src","https://www.codefactory.wien/wp-content/uploads/2018/03/codefactory-vienna-footer-h.png");
+$("blockquote").css({"background-color":"orange","font-style":"italic"});
+$("h1#logo").css("background-color","black");
+$("span.gray").css("background-color","white");
