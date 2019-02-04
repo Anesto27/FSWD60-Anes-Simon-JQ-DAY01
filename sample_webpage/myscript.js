bd5fe236a7401bb7c240352874886eb44a960101
Anesto27 <anes.2503@gmx.at>
2019-02-04 11:50:11 +0100

    advanced exercise from 1-4 finished

---
+++ sample_webpage/myscript.js
@@ -0,0 +1,9 @@
+//1
+$("a[href]").attr("href","https://www.codefactory.wien" );
+//2
+$("code").css("color","red");
+//3
+$("ol>li").css("background-color","#2a7b90");
+//4
+$("form textarea").attr("placeholder","Express your opinion");
+//5
